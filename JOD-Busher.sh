20fc03fdaa9eda187746abfc700ebd2662602514
Rizwan Syed <78642744+mr-rizwan-syed@users.noreply.github.com>
2021-12-07 12:48:08 +0530

    Add files via upload

--- JOD-Busher.sh
+++ JOD-Busher.sh
@@ -27,15 +27,14 @@ trap "trap_ctrlc" 4
 
 banner(){
     echo '
-    
-     ██╗ ██████╗ ██████╗      ██╗  ██╗██╗     ██████╗  █████╗ 
-     ██║██╔═══██╗██╔══██╗     ╚██╗██╔╝██║     ██╔══██╗██╔══██╗
-     ██║██║   ██║██║  ██║█████╗╚███╔╝ ██║     ██████╔╝╚█████╔╝
-██   ██║██║   ██║██║  ██║╚════╝██╔██╗ ██║     ██╔══██╗██╔══██╗
-╚█████╔╝╚██████╔╝██████╔╝     ██╔╝ ██╗███████╗██║  ██║╚█████╔╝
- ╚════╝  ╚═════╝ ╚═════╝      ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ╚════╝                                                               
 
-        '
+     ██  ██████  ██████        ██████  ██    ██ ███████ ██   ██ ███████ ██████  
+     ██ ██    ██ ██   ██       ██   ██ ██    ██ ██      ██   ██ ██      ██   ██ 
+     ██ ██    ██ ██   ██ █████ ██████  ██    ██ ███████ ███████ █████   ██████  
+██   ██ ██    ██ ██   ██       ██   ██ ██    ██      ██ ██   ██ ██      ██   ██ 
+ █████   ██████  ██████        ██████   ██████  ███████ ██   ██ ███████ ██   ██ 
+                                                                                
+                                                                                                                                                     '
 }
 ####Add Functions Here
 
@@ -47,22 +46,17 @@ function checker(){
         test -f "$project/sub-url-stripped.txt"
     }
 
-    is_sd_snap_checker(){
-        test -d "$project/sd-screenshot"
+    is_massdns_checker(){
+        test -f "$project/ips.txt"
     }
-
-    is_webportscan_checker(){
-        test -f "$project/$project-webportscan.txt"
-    }
-
+    
     is_uniqueparameter_checker(){
         test -f "$project/$URL/uniqueparam.txt"
     }
-    is_uniqueparameter_checker(){
-        test -f "$project/$URL/qsinject.txt"
-    }
 }
+
 function subdomains(){
+    read -p "${RED}Domain: ${RESET}" domain && echo -e
     mkdir -p $project/subdomains
     subfinder -silent -d $domain > $project/subdomains/subdomains.txt
     cat $project/subdomains/subdomains.txt | httpx -o $project/subdomains/sd-httpx.txt
@@ -89,15 +83,86 @@ function find_ips(){
     massdnsOutput=$project/ips.txt
     allSubdomainsOutput=$project/subdomains/subdomains.txt
     massdns_temp=$project/massdns.tmp
+    #massdnsOutput
+    if is_massdns_checker; then
+    echo "MassDns Result Exist Already" || return
+    else
+        continue
+    fi
     massdns -r $resolversFile -t A -w $massdns_temp $allSubdomainsOutput
     cat $massdns_temp | cut -d " " -f3 | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort -u >> $massdnsOutput
     rm $massdns_temp
     echo -e "Massdns complete"
 }
 
+function find_ips_start(){
+    if is_massdns_checker; then
+    echo "MassDns Result Exist Already" || return
+    else
+        find_ips
+    fi
+}
+
+parametercrawler(){
+    
+    runpc(){
+    
+        echo Scannning $URL
+        mkdir -p $project/$URL
+        mkdir -p $project/$URL/gf-param
+        ##waybackurl-gau
+        echo -e
+        waybackurls $URL > $project/$URL/all-urls.log
+        gau $URL >> $project/$URL/all-urls.log
+        cat $project/$URL/all-urls.log | sort -u > $project/$URL/all-urls.txt
+        cat $project/$URL/all-urls.txt
+        rm $project/$URL/all-urls.log
+        echo "[${GREEN}I${RESET}] Done with Waybackurls and Gau${RESET}"
+               
+        #Stripping
+        echo -e
+        cat $project/$URL/all-urls.txt | grep "=" | egrep -iv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt|js)" > $project/$URL/uniqueparam.txt
+        echo "[${GREEN}I${RESET}]Extracting URL with Valid Parameters${RESET}"
+        cat $project/$URL/all-urls.txt | qsinject -i 'FUZZ' -iu -decode > $project/$URL/qsinjected.txt
+
+        #gf-patterns
+        #Some pattern may find sensitive info that's why string not replaced
+        cat $project/$URL/all-urls.txt | gf xss | qsinject -i 'FUZZ' -iu -decode | anew -q $project/$URL/gf-param/xss.txt
+        cat $project/$URL/all-urls.txt | gf sqli | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/sqli.txt
+        cat $project/$URL/all-urls.txt | gf ssrf | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/ssrf.txt
+        cat $project/$URL/all-urls.txt | gf ssti | qsinject -i '"FUZZ"' -iu -decode | anew -q  $project/$URL/gf-param/ssti.txt
+        cat $project/$URL/all-urls.txt | gf redirect | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/redirect.txt
+        cat $project/$URL/all-urls.txt | gf lfi | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/lfi.txt
+        cat $project/$URL/all-urls.txt | gf rce | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/rce.txt
+        cat $project/$URL/all-urls.txt | gf upload-fields | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/upload-fields.txt
+        cat $project/$URL/all-urls.txt | gf interestingparams | anew -q $project/$URL/gf-param/interestingparams.txt
+        cat $project/$URL/all-urls.txt | gf interestingEXT | anew -q $project/$URL/gf-param/interestingEXT.txt
+        cat $project/$URL/all-urls.txt | gf img-traversal | anew -q $project/$URL/gf-param/img-traversal.txt
+        cat $project/$URL/all-urls.txt | gf php-sources | anew -q $project/$URL/gf-param/php-sources.txt
+        cat $project/$URL/all-urls.txt | gf s3-buckets | anew -q $project/$URL/gf-param/s3-buckets.txt
+        cat $project/$URL/all-urls.txt | gf servers | anew -q $project/$URL/gf-param/servers.txt
+        cat $project/$URL/all-urls.txt | gf typos | anew -q $project/$URL/gf-param/typos.txt
+        find $project/$URL/gf-param/ -type f -empty -print -delete
+
+    }
+    
+    upcvalidator(){
+        
+        if is_uniqueparameter_checker; then
+        echo "Parameter Results Exist Already for $URL" || return
+        else
+            runpc
+        fi
+    }
+
+    [ -z "$URL" ] && askurl || upcvalidator
+    echo Got this $URL
+    
+}
 
 askurl(){
         read -p "${RED}URL: ${RESET}" URL && echo -e
+        parametercrawler
     }
 
 function choicemaker(){
@@ -116,93 +181,29 @@ function choicemaker(){
     URL=$d
 }
 
-parametercrawler(){
-    
-    runpc(){
-    
-        echo Scannning $URL
-        mkdir -p $project/$URL
-        mkdir -p $project/$URL/gf-param
-        ##waybackurl-gau
-        echo -e
-        waybackurls $URL > $project/$URL/all-urls.log
-        gau $URL >> $project/$URL/all-urls.log
-        cat $project/$URL/all-urls.log | sort -u > $project/$URL/all-urls.txt
-        cat $project/$URL/all-urls.txt
-        echo "[${GREEN}I${RESET}] Done with Waybackurls and Gau${RESET}"
-               
-        #Stripping
-        echo -e
-        cat $project/$URL/all-urls.txt | grep "=" | egrep -iv ".(jpg|jpeg|gif|css|tif|tiff|png|ttf|woff|woff2|ico|pdf|svg|txt|js)" > $project/$URL/uniqueparam.txt
-        echo "[${GREEN}I${RESET}]Extracting URL with Valid Parameters${RESET}"
-        cat $project/$URL/all-urls.txt | qsinject -i '"FUZZ"' -iu -decode > $project/$URL/qsinjected.txt
-
-        #gf-patterns
-        #Some pattern may find sensitive info that's why string not replaced
-        cat $project/$URL/all-urls.txt | gf xss | qsinject -i 'FUZZ' -iu -decode | anew -q $project/$URL/gf-param/xss.txt
-        cat $project/$URL/all-urls.txt | gf sqli | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/sqli.txt
-        cat $project/$URL/all-urls.txt | gf ssrf | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/ssrf.txt
-        cat $project/$URL/all-urls.txt | gf ssti | qsinject -i '"FUZZ"' -iu -decode | anew -q  $project/$URL/gf-param/ssti.txt
-        cat $project/$URL/all-urls.txt | gf redirect | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/redirect.txt
-        cat $project/$URL/all-urls.txt | gf lfi | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/lfi.txt
-        cat $project/$URL/all-urls.txt | gf rce | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/rce.txt
-        cat $project/$URL/all-urls.txt | gf upload-fields | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/upload-fields.txt
-        cat $project/$URL/all-urls.txt | gf http-auth | anew -q $project/$URL/gf-param/http-auth.txt
-        cat $project/$URL/all-urls.txt | gf interestingparams | anew -q $project/$URL/gf-param/interestingparams.txt
-        cat $project/$URL/all-urls.txt | gf interestingEXT | anew -q $project/$URL/gf-param/interestingEXT.txt
-        cat $project/$URL/all-urls.txt | gf img-traversal | anew -q $project/$URL/gf-param/img-traversal.txt
-        cat $project/$URL/all-urls.txt | gf php-sources | anew -q $project/$URL/gf-param/php-sources.txt
-        cat $project/$URL/all-urls.txt | gf s3-buckets | anew -q $project/$URL/gf-param/s3-buckets.txt
-        cat $project/$URL/all-urls.txt | gf sec | anew -q $project/$URL/gf-param/sec.txt
-        cat $project/$URL/all-urls.txt | gf secrets | anew -q $project/$URL/gf-param/secrets.txt
-        cat $project/$URL/all-urls.txt | gf servers | anew -q $project/$URL/gf-param/servers.txt
-        cat $project/$URL/all-urls.txt | gf strings | anew -q $project/$URL/gf-param/typos.txt
-        find $project/$URL/gf-param/ -type f -empty -print -delete
-
-    }
-    
-    upcvalidator(){
-        is_uniqueparameter_checker(){
-        test -f "$project/$URL/uniqueparam.txt"
-        }
-
-        if is_uniqueparameter_checker; then
-        echo "Unique Parameter File Exist Already" || return
-        else
-            runpc
-        fi
-    }
-
-    [ -z "$URL" ] && askurl || upcvalidator
-    echo Got this $URL
-    
-}
-
-
-startp(){
+function startp(){
     while true; do
-    choicemaker
-    parametercrawler
-    again
-    done
-}
-
-Again(){
-        while true; do
-        read -p "Do you want to run it again on each subdomains [y/n]?" yn
+        choicemaker
+        parametercrawler
+        read -p "${BLUE}Do you want to run it again on each subdomains ${RESET}[y/n]?" yn
         case $yn in
             [Yy]* ) startp; break;;
             [Nn]* ) break;;
             * ) echo "Please answer yes or no.";;
         esac
-        done
+    done
+}
+
+
+function singleurlparam(){
+    parametercrawler
 }
 
 #Menu options
 options[0]="Gather Subdomains"
 options[1]="Resolve IP's"
 options[2]="Parameter Crawler"
-options[3]="Check for Open Redirect"
+options[3]="Run on single URL"
 options[4]="Advance Vulnerability Scan"
 
 #Variables
@@ -245,15 +246,15 @@ function ACTIONS {
     fi
     if [[ ${choices[1]} ]]; then
         echo "[2] Finding IP Addresses"
-        find_ips
+        find_ips_start
     fi
     if [[ ${choices[2]} ]]; then
-        echo "[3] Option 1 selected; Need a Single URL"
+        echo "[3] Option 1 selected; Parameter Crawler"
 	    startp
     fi
     if [[ ${choices[3]} ]]; then
         echo "Option 4 selected"
-        rustscan1
+        singleurlparam
     fi
 }
 
@@ -268,7 +269,7 @@ projectdirectorycheck(){
     else
         mkdir -p $project
     fi
-    read -p "${RED}Domain: ${RESET}" domain && echo -e
+    
 }
 
 checker
