e9a5643295b34b6c554630df291e1690018c7aa9
Rizwan Syed <78642744+mr-rizwan-syed@users.noreply.github.com>
2021-12-09 20:41:28 +0530

    Add files via upload

--- JOD-Busher.sh
+++ JOD-Busher.sh
@@ -131,7 +131,7 @@ parametercrawler(){
 
         #gf-patterns
         #Some pattern may find sensitive info that's why string not replaced
-        cat $project/$URL/all-urls.txt | gf xss | qsinject -i 'FUZZ' -iu -decode | anew -q $project/$URL/gf-param/xss.txt
+        cat $project/$URL/all-urls.txt | gf xss | qsinject -i '' -iu -decode | anew -q $project/$URL/gf-param/xss.txt
         cat $project/$URL/all-urls.txt | gf sqli | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/sqli.txt
         cat $project/$URL/all-urls.txt | gf ssrf | qsinject -i 'FUZZ' -iu -decode | anew -q  $project/$URL/gf-param/ssrf.txt
         cat $project/$URL/all-urls.txt | gf ssti | qsinject -i '"FUZZ"' -iu -decode | anew -q  $project/$URL/gf-param/ssti.txt
