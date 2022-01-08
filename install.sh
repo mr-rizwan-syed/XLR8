094e722c5f5728f7d574516e34c283d2dcc73339
Rizwan Syed <78642744+mr-rizwan-syed@users.noreply.github.com>
2022-01-08 22:16:52 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+go install github.com/ameenmaali/qsinject@latest
+go install github.com/tomnomnom/gf@latest
+go install github.com/hahwul/dalfox/v2@latest
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install github.com/tomnomnom/waybackurls@latest
+go install github.com/tomnomnom/qsreplace@latest
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+go install github.com/tomnomnom/anew@latest
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install github.com/haccer/subjack@latest
